e14c5f23c2224237a1ee7a7f83694889145fa6c3
Isa <Isa@gmail.com>
2025-11-07 22:32:30 +0500

    l10

--- app/src/main/java/com/example/lab9/MainActivity.java
+++ app/src/main/java/com/example/lab9/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.lab9;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -26,8 +30,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text1);
-        registerForContextMenu(textView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setMessage("Вы действительно хотите выйти?");
+            dialog.setCancelable(false);
+
+            dialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    MainActivity.this.finish();
+                }
+            });
+
+            dialog.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    dialogInterface.cancel();
+                }
+            });
+
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+        });
+
+
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -36,35 +66,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        getMenuInflater().inflate(R.menu.contextmenu, menu);
-        super.onCreateContextMenu(menu, v, menuInfo);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.text1) {
-            if (item.isChecked()) {
-                TextView textView = findViewById(R.id.text1);
-                textView.setVisibility(TextView.VISIBLE);
-                item.setChecked(false);
 
-            } else {
-                TextView textView = findViewById(R.id.text1);
-                textView.setVisibility(TextView.INVISIBLE);
-                item.setChecked(true);
-
-            }
-        }
-        if (id == R.id.about) {
-
-            Toast.makeText(MainActivity.this, R.string.fio, Toast.LENGTH_LONG).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,18 +77,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        TextView textView = findViewById(R.id.text1);
-
-        if (id == R.id.color_red) {
-            textView.setTextColor(Color.parseColor("red"));
-        } else if (id == R.id.color_black) {
-            textView.setTextColor(Color.parseColor("black"));
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        try {
+            dialog.setMessage(getTitle().toString() + " версия " +
+                    getPackageManager().getPackageInfo(getPackageName(), 0).versionName +
+                    "\r\n\nПрограмма с примером выполнения диалогового окна" +
+                    "\r\n\nАвтор - Баркинхоев Иса , гр. ИС-34");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
         }
 
-        return super.onContextItemSelected(item);
+        dialog.setTitle("О программе");
+
+        dialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setIcon(R.mipmap.ic_launcher_round);
+
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
+
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
